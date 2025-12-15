b812de5c96f81eacee1ec585bbca413221d3c73a
yusaku12 <sagiri0702@gmail.com>
2025-12-15 23:34:01 +0900

    Sceneを追加

---
+++ Source/Scene/Scene.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+class Scene
+{
+public:
+
+private:
+
+    //! シーンデータ(GPUで送る)
+    struct SceneData
+    {
+        Matrix view;        //!< ビュー行列
+        Matrix projection;  //!< プロジェクト行列
+        Vector3 eye;        //!< カメラ位置
+    };
+};
